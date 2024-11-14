8b90ba818396358b73e18d96b1a5a15448bfb0b2
Sawyron <s4pakvv@gmail.com>
2024-11-14 18:56:36 +0700

    initial commit

---
+++ src/main/java/com/gigastore/GigaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.gigastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigaStoreApplication.class, args);
+	}
+
+}
