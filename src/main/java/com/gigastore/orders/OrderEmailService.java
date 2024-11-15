efdc6945aec893ebdd9864998d3d7e5019091c6f
Sawyron <s4pakvv@gmail.com>
2024-11-15 19:21:16 +0700

    implement OrderEmailService

--- src/main/java/com/gigastore/orders/OrderEmailService.java
+++ src/main/java/com/gigastore/orders/OrderEmailService.java
@@ -6,9 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
-
-import java.util.HashMap;
 
 public class OrderEmailService {
     private static final Logger log = LoggerFactory.getLogger(OrderEmailService.class);
@@ -21,20 +20,30 @@ public class OrderEmailService {
 
     @Async
     public void sendOrderConfirmationEmail(int customerId, int orderId) {
-        var email = new HashMap<String, String>();
-        email.put("to", "customer %s@example.com".formatted(customerId));
-        email.put("subject", "Order Confirmation");
-        email.put("body", "Order confirmation for order ID: %s".formatted(orderId));
-        ResponseEntity<String> responseEntity = client.post()
+        try {
+            ResponseEntity<String> responseEntity = sendRequest(customerId, orderId);
+            if (responseEntity.getStatusCode().isSameCodeAs(HttpStatus.OK)) {
+                log.info("successfully send email. Response: {}", responseEntity);
+            } else {
+                log.error("Failed to send order confirmation email. Response Code: {}", responseEntity.getStatusCode());
+            }
+        } catch (Exception e) {
+            // TODO handle sending error
+            log.error("cannot send order confirmation request");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ResponseEntity<String> sendRequest(int customerId, int orderId) {
+        var email = new LinkedMultiValueMap<String, String>();
+        email.add("to", "customer %s@example.com".formatted(customerId));
+        email.add("subject", "Order Confirmation");
+        email.add("body", "Order confirmation for order ID: %s".formatted(orderId));
+        return client.post()
                 .uri("/send")
                 .body(email)
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .retrieve()
                 .toEntity(String.class);
-        if (responseEntity.getStatusCode().isSameCodeAs(HttpStatus.OK)) {
-            log.info("successfully send email. Response: {}", responseEntity);
-        } else {
-            log.error("Failed to send order confirmation email. Response Code: {}", responseEntity.getStatusCode());
-        }
     }
 }
